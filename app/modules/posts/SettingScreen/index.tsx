21ac770d57328f6d772d01d67a10bf805d660f48
dylan <devdesarrollo96@gmail.com>
2024-11-08 21:35:11 -0500

    configuracion en settings

--- app/modules/posts/SettingScreen/index.tsx
+++ app/modules/posts/SettingScreen/index.tsx
@@ -1,72 +1,61 @@
 import React, { FC } from "react"
 import * as Application from "expo-application"
-import { Platform, TextStyle, View, ViewStyle } from "react-native"
+import { Platform, StyleSheet, TextStyle, TouchableOpacity, View, ViewStyle } from "react-native"
 
-
+import { Drawer } from "react-native-drawer-layout"
 
 import { useStore } from "app/store/useStore"
 import { TabScreenProps } from "app/navigators/TabNavigator"
 import { Button, ListItem, Screen, Text } from "app/components"
 import { colors, spacing } from "app/theme"
 import { isRTL } from "app/i18n"
+import { DrawerIconButton } from "app/screens/DemoShowroomScreen/DrawerIconButton"
+import { X } from "lucide-react-native"
 
 /**
  * @param {string} url - The URL to open in the browser.
  * @returns {void} - No return value.
  */
 
-
-export const SettingScreen: FC<TabScreenProps<"Settings">> = function SettingScreen(
-  _props,
-) {
-
-
-  const {setRemoveSession} = useStore()
-
-  //const usingHermes = typeof HermesInternal === "object" && HermesInternal !== null
-  // @ts-expect-error
-  const usingFabric = global.nativeFabricUIManager != null
-
-/*  const demoReactotron = React.useMemo(
-    () => async () => {
-      if (__DEV__) {
-        console.tron.display({
-          name: "DISPLAY",
-          value: {
-            appId: Application.applicationId,
-            appName: Application.applicationName,
-            appVersion: Application.nativeApplicationVersion,
-            appBuildVersion: Application.nativeBuildVersion,
-            hermesEnabled: usingHermes,
-          },
-          important: true,
-        })
-      }
-    },
-    [],
-  )*/
-   // <Button style={$button} tx="demoDebugScreen.reactotron" onPress={demoReactotron} />
+export const SettingScreen: FC<TabScreenProps<"Settings">> = (_props) => {
+  const { setRemoveSession } = useStore()
+  const [open, setOpen] = React.useState(false)
 
   return (
-    <Screen preset="scroll" safeAreaEdges={["top"]} contentContainerStyle={$container}>
-      <Text style={$title} preset="heading" tx="settingScreen.title" />
-      <View style={$itemsContainer}>
-        <ListItem
-          LeftComponent={
-            <View style={$item}>
-              <Text preset="bold">App Id</Text>
-              <Text>{Application.applicationId}</Text>
-            </View>
-          }
-        />
+    <Drawer
+      open={open}
+      onOpen={() => setOpen(true)}
+      onClose={() => setOpen(false)}
+      drawerType="front"
+      drawerPosition="right"
+      drawerStyle={{right:0}}
+      renderDrawerContent={() => {
+        return (
+          <TouchableOpacity onPress={() => setOpen(false)} style={ {top:30}}>
+            <X size={30} color='black' style={{alignSelf:'flex-end', right:10}} />
+          </TouchableOpacity>
+        )
+      }}
+    >
+      <View style={ {top:25, backgroundColor: colors.background }}>
+          <View style={{ alignSelf:'flex-end'}}>
+            <DrawerIconButton onPress={() => setOpen((prevOpen) => !prevOpen)}/>
+          </View>
       </View>
-      <View style={$buttonContainer}>
-        <Button style={$button} tx="common.logOut" onPress={() => setRemoveSession()} />
-      </View>
-    </Screen>
+    </Drawer>
   )
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+  },
+  buttons: {
+    gap: 8,
+  },
+})
 const $container: ViewStyle = {
   paddingTop: spacing.lg + spacing.xl,
   paddingBottom: spacing.xxl,
